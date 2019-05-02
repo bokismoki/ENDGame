e43fd5ece911bc2e7f08714b8ec79e9aeb9f5a64
bokismoki <borisbosnjak084@gmail.com>
2019-05-02 17:33:53 +0200

    ENDGame Template recreation, responsive

---
+++ script.js
@@ -0,0 +1,9 @@
+var hamburger = document.querySelector(".hamburger");
+var menuToggler = document.querySelector(".menuToggler");
+
+
+hamburger.addEventListener("click", function() {
+
+    menuToggler.classList.toggle("hide");
+
+});
